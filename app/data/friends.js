aed7dd76e4d3529e319023505dc82011df2fc632
timkomo <timko.matthew@gmail.com>
2018-08-04 12:05:39 -0400

    added css

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+// Data structure that will store the friends data
+var musicMatchesArray = [
+	{
+		name: 'Mark',
+		photo: 'https://thumb9.shutterstock.com/display_pic_with_logo/257323/257323,1226395297,12/stock-photo-punk-guy-with-mohawk-20344747.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'3',
+			'5',
+			'4',
+			'1',
+			'3',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Jenna',
+		photo: 'https://previews.123rf.com/images/massonforstock/massonforstock1708/massonforstock170800572/83440137-young-girl-in-indie-style-clothes-with-notebook-on-green-grass-in-the-park.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Ian',
+		photo: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX7846456.jpg',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Victoria',
+		photo: 'https://media.istockphoto.com/photos/nerd-girl-picture-id465441526',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'3',
+			'2',
+			'4',
+			'4'
+		]
+	},
+	
+];
+
+module.exports = musicMatchesArray;
